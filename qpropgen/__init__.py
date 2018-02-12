21d83a876413c8be29ff3fbf69da18cdf192c5df
Aurelien Gateau <mail@agateau.com>
2018-02-12 19:27:19 +0100

    Improve description

--- qpropgen/__init__.py
+++ qpropgen/__init__.py
@@ -3,5 +3,5 @@ __version__ = '0.1.0'
 __license__ = 'Apache 2.0'
 
 DESCRIPTION = """\
-Generate QML property-based headers and implementation
+Generate a QML-friendly QObject-based C++ class from a class definition file.
 """
