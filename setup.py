4dcb0b23e5a1afda46a0d8e5704b2bb1ffa75c7c
Aurélien Gâteau <mail@agateau.com>
2018-12-08 19:56:05 +0100

    Switch to a declarative setup

--- setup.py
+++ setup.py
@@ -1,31 +1,3 @@
 #!/usr/bin/env python3
 from setuptools import setup
-
-import qpropgen
-
-setup(
-    name=qpropgen.__appname__,
-    version=qpropgen.__version__,
-    description=qpropgen.DESCRIPTION,
-    author='Aurélien Gâteau',
-    author_email='mail@agateau.com',
-    license=qpropgen.__license__,
-    platforms=['any'],
-    packages=['qpropgen'],
-    package_data={
-        'qpropgen': ['templates/template.h', 'templates/template.cpp']
-    },
-    install_requires=['PyYAML', 'jinja2'],
-    entry_points={
-        'console_scripts': [
-            'qpropgen = qpropgen.main:main',
-        ],
-    },
-    classifiers=[
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3',
-        'Topic :: Software Development :: Build Tools',
-    ]
-)
+setup()
